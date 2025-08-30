8c22a27b16e643577ae1e2c29a1340d84aa1668c
Jan Trejtnar <honza.trejtnar@seznam.cz>
2025-08-30 16:28:24 +0200

    Zhotovení a optimalizace to-do list aplikace.

--- script.js
+++ script.js
@@ -1,64 +1,103 @@
 $(document).ready(function() {
     // Inicializuj a naplň proměnné
     tasks = localStorage.getItem("tasks") === null ? [] : JSON.parse(localStorage.getItem("tasks"));
-    alert("tasks array: " + tasks + " localStorage-getItem: " + localStorage.getItem("tasks"))
     const taskList = $("#task-list");
 
-    renderTasks(tasks);
+    // Vykresli tasky do HTML
+    for (let task = 0; task < tasks.length; task++) {
+        appendTask(tasks[task]);
+    }
+
+
+
+    /* EVENT LISTENERS */
 
     // Přidání tasku po kliknutí na tlačítko
     $("#add-task").click(function() {
         createTask();
     })
 
+    // Zaškrtnutí tasku po kliknutí na task-text
+    $("#task-list").on("click", ".task-checkbox, .task-text", function() {
+        const id = $(this).closest("li").attr("data-id");
+        checkTask(id);
+    });
+
     // Odebrání tasku po kliknutí na "Delete" tlačítko
     $("#task-list").on("click", ".delete-task", function() {
-        deleteTask($(this).attr("data-index"));
+        const id = $(this).closest("li").attr("data-id");
+        deleteTask(id);
+        $(this).closest("li").remove();
     });
 
 
-    // Vykresli tasky do HTML
-    function renderTasks (arr) {
-        for (let task = 0; task < arr.length; task++) {
-            taskList.
-                append(
-                    `<li>
-                        <input type="checkbox" class="task-checkbox" />
-                        <span class="task-text">${arr[task]}</span>
-                        <button class="delete-task" data-index="${task}">Delete</button>
-                    </li>`
-                )
-        }
-        console.log(tasks);
-    }
 
-    // Vytvoření nového tasku a uložení do localStorage
+
+    /* FUNKCE */
+
+    // Vytvoření nového tasku
     function createTask () {
-        const newTask = $("#new-task").val()
+        const newTaskTitle = $("#new-task").val();
 
-        if (!newTask) {
+        if (!newTaskTitle) {
             alert("Input can not be empty. Enter some task!");
             return;
-        } else {
-            tasks.push(newTask);
-            localStorage.setItem("tasks", JSON.stringify(tasks));
-            clearInput();
-
-            renderTasks([newTask]);
         }
+        const newTask = { 
+            id: Date.now(),
+            title: newTaskTitle,
+            checked: false
+        };
+
+        tasks.push(newTask);
+
+        saveData();
+        clearInput();
+        appendTask(newTask);
     }
 
     // Smazání tasku
-    function deleteTask (taskIndex) {
-        alert("Deleting task with index: " + taskIndex);
-        tasks.splice(taskIndex, 1);
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-        taskList.empty();
-        renderTasks(tasks);
+    function deleteTask (taskId) {
+        tasks = tasks.filter(t => t.id != taskId);
+        saveData();
+    }
+
+    // Zaškrtnutí tasku
+    function checkTask (taskId) {
+        const task = tasks.find(t => t.id == taskId);
+        if (!task) return;
+
+        task.checked = !task.checked;
+
+        const listItem = $("#task-list").find(`li[data-id='${taskId}']`);
+        listItem.toggleClass("checked");
+        listItem.find('.task-checkbox').prop('checked', task.checked);
+
+        saveData();
     }
 
     // Vyčištění inputu 
     function clearInput () {
         $("#new-task").val("");
     }
+
+    // Uložení do localStorage
+    function saveData () {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
+    // Připoj task do listu
+    function appendTask(task) {
+        taskList.append(`
+            <li data-id="${task.id}" class="${task.checked ? "checked" : ""}">
+                <input type="checkbox" class="task-checkbox" ${task.checked ? "checked" : ""} />
+                <span class="task-text">${task.title}</span>
+                <button class="delete-task"><i class="material-icons">delete</i></button>
+            </li>
+        `);
+    }
+
+
+
+
 })
